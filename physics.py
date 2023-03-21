184ad0059d8add16a87c1fbef5e797e9c08dfff6
AlexMC1997 <98240120+AlexMC1997@users.noreply.github.com>
2023-03-21 14:04:59 -0400

    thing

--- physics.py
+++ physics.py
@@ -1,9 +1,16 @@
+import matplotlib.pyplot as pt
 P = (0, 10)
 V0 = (0, 0)
 
 def do_phys(p, v0, dt, t0, t1, g):
     t = t0
+    tab = []
     while t <= t1:
-        print(f't = {t:.2f}: ({p[0] + v0[0]*t:8.3f}, {p[1] + v0[1]*t + 0.5 * (-g) * t**2:8.3f})')
+        pos = (p[0] + v0[0]*t, p[1] + v0[1]*t + 0.5 * (-g) * t**2)
+        print(f'({t:.2f}, {p[0] + v0[0]*t:7.3f}, {p[1] + v0[1]*t + 0.5 * (-g) * t**2:7.3f})')
+        tab.append(pos)
         t += dt
-do_phys(P, V0, 0.2, 0, 2, 10)
+    return tab
+tab = do_phys(P, V0, 0.2, 0, 2, 10)
+tab = list(zip(*tab))
+pt.plot(tab[0], tab[1], '-r')
