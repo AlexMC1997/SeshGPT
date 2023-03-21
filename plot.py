180bd0b7b2c5949196373f2580b27fe3610af06c
CyrusTheVirus <cyrusmajd2001@gmail.com>
2023-03-21 14:25:44 -0400

    insane simulations

--- plot.py
+++ plot.py
@@ -18,34 +18,49 @@ def extract(filename):
     
     return data
 
-def plot():
-    # x-axis values
-    x = [1,2,3,4,5,6,7,8,9,10]
-    # y-axis values
-    y = [2,4,5,7,6,8,9,11,12,12]
+def plot(time1, x1, y1, time2, x2, y2):
+    # first dataset
+    plt.plot(time1, y1, color='green', linestyle='dashed', label= "Kinematics Calculated Result",
+         marker='.', markerfacecolor='green')
     
-    # plotting points as a scatter plot
-    plt.scatter(x, y, label= "stars", color= "green", 
-                marker= "*", s=30)
+    # second dataset
+    plt.plot(time2, y2, label= "GPT Calculated Result", color= "blue", linestyle='dashed', 
+                marker= ".")
     
     # x-axis label
-    plt.xlabel('x - axis')
-    # frequency label
-    plt.ylabel('y - axis')
+    plt.xlabel('Time')
+    # y-axis label
+    plt.ylabel('Position')
     # plot title
-    plt.title('My scatter plot!')
+    plt.title('Position over time')
     # showing legend
     plt.legend()
     
     # function to show the plot
     plt.show()
 
+def print_extracted(data):
+    for time, x, y in data:
+        print(f"Time: {time}, X: {x}, Y: {y}")
+    print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+
+def extract_from_tuple(data):
+    time_arr = []
+    x_coords = []
+    y_coords = []
+    for time, x, y in data:
+        time_arr.append(time)
+        x_coords.append(x)
+        y_coords.append(y)
+    return time_arr, x_coords, y_coords
+
 calculated_data = extract("calculated.txt")
 gpt_data = extract("gpt_generated.txt")
 
-for time, x, y in calculated_data:
-    print(f"Time: {time}, X: {x}, Y: {y}")
+print_extracted(calculated_data)
+print_extracted(gpt_data)
 
-print("======================")
-for time, x, y in gpt_data:
-    print(f"Time: {time}, X: {x}, Y: {y}")
+calculated_time, calculated_x, calculated_y = extract_from_tuple(calculated_data)
+gpt_time, gpt_x, gpt_y = extract_from_tuple(gpt_data)
+
+plot(calculated_time, calculated_x, calculated_y, gpt_time, gpt_x, gpt_y)
